aa4e6d05269a290077c1fae14cedb6d7e3660f7a
AlfredASC6 <alfredfrac99@gmail.com>
2023-05-30 18:41:39 -0400

    Added updated version with react and dijkstra algorithm visualizer

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import AlgoVis from './algovis/algovis'
+function App() {
+  return (
+    <div className="App">
+      <AlgoVis></AlgoVis>
+    </div>
+  );
+}
+
+export default App;
